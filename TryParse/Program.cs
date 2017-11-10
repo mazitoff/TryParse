d08fc6cab99ed5b56c3fcbfcf0916d1114238fa9
mazitov <mazitov@ukr.net>
2017-11-10 21:18:43 +0200

    Realise

--- TryParse/Program.cs
+++ TryParse/Program.cs
@@ -8,8 +8,21 @@ namespace TryParse
 {
     class Program
     {
+        static bool TryParse(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((int)str[i] < 47 || (int)str[i] > 58)
+                    return false;
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
+            bool res = TryParse(Console.ReadLine());
+            Console.WriteLine(res);
+            Console.ReadKey();
         }
     }
 }
